a5982fc4bc11c4b4dc97aca20d1bc0e8f3cdb414
Jakub Trtik <jakubtrtik2@gmail.com>
2021-03-23 06:15:38 +0100

    Added Google analytics Contact form required fields and toast

--- src/components/ContactMail.vue
+++ src/components/ContactMail.vue
@@ -1,12 +1,24 @@
 <template>
   <b-form @submit.prevent="sendEmail">
-    <b-form-input name="email" v-model="email" placeholder="Email" />
-    <b-form-input name="name" v-model="name" class="mt-3" placeholder="Jméno" />
+    <b-form-input
+      name="email"
+      v-model="email"
+      placeholder="Email"
+      :required="true"
+    />
+    <b-form-input
+      name="name"
+      v-model="name"
+      class="mt-3"
+      placeholder="Jméno"
+      :required="true"
+    />
     <b-textarea
       name="content"
       v-model="content"
       class="mt-3"
       placeholder="Text"
+      :required="true"
     />
     <b-col class="mt-3 text-center">
       <b-button type="submit" variant="primary">Odeslat</b-button>
@@ -28,6 +40,15 @@ export default {
   },
   methods: {
     sendEmail(e) {
+      if (this.name === "") {
+        return;
+      }
+      if (this.email === "") {
+        return;
+      }
+      if (this.content === "") {
+        return;
+      }
       try {
         emailjs.sendForm(
           "service_tlrva1f",
@@ -41,8 +62,18 @@ export default {
           }
         );
       } catch (error) {
+        this.$bvToast.toast(`Email haven't been sent`, {
+          title: "Email sent fail",
+          variant: "danger",
+          autoHideDelay: 5000
+        });
         console.log({ error });
       }
+      this.$bvToast.toast(`Email sent`, {
+        title: "Email sent success",
+        variant: "success",
+        autoHideDelay: 5000
+      });
       this.name = "";
       this.email = "";
       this.content = "";
